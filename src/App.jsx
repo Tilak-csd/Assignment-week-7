129b31e260255dfd07ccc8bb12c571f1ebd706e3
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-25 16:27:35 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Profile from './components/Profile'
 import ParagraphGenerator from './components/ParagraphGenerator'
-
+import Card from './components/Card'
 
 export default function App() {
   return (
     <div>
       {/* <Profile /> */}
+      <Card></Card>
 
-
-      <ParagraphGenerator />
+      {/* <ParagraphGenerator /> */}
 
     </div>
   )
