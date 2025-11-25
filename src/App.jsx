b5c71a563d20b175fc4ae2ccd0ac0c6919da446c
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-25 08:00:30 +0545

    fourth assignment completed where i create a paragraph generation app where the user input the number of word, a paragraph is generated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Profile from './components/Profile'
 import ParagraphGenerator from './components/ParagraphGenerator'
 
+
 export default function App() {
   return (
     <div>
       {/* <Profile /> */}
+
+
       <ParagraphGenerator />
+
     </div>
   )
 }
--- src/components/ParagraphGenerator.jsx
+++ src/components/ParagraphGenerator.jsx
@@ -1,25 +1,45 @@
-import React, { useState } from 'react'
-import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { NumWords } from '../store/atoms/main'
+import { useEffect, useState } from 'react'
 
-export default function ParagraphGenerator() {
-    
-    return (
-        <div>
-            <RecoilRoot>
-                <Input />
-            </RecoilRoot>
-        </div>
-    )
-}
-
-function Input() {
+function Input({onGenerate}) {
     const setWords = useSetRecoilState(NumWords)
-    const word = useRecoilValue(NumWords)
-    console.log(word);
     return <div>
         <p>Para Generator</p>
-        <input type="number" onChange={(e) => setWords(e.target.value)} placeholder='Enter the Number of words.' />
-        <button >Generate</button>
+        <input type="number" onChange={(e) => setWords(Number(e.target.value))} placeholder='Enter the Number of words.' />
+        <button onClick={onGenerate} >Generate</button>
     </div>
+}
+
+function ParagraphDisplay({generateTriger}) {
+    const wordNumber = useRecoilValue(NumWords)
+    const [paragraph, setParagraph] = useState([])
+    const wordLists = ["hello", "is", "hii", "my", "name", "tilak", "i", "am", "developer"]
+    useEffect(() => {
+        if (wordNumber > 0){
+            let paragraph = []
+            for (let i = 0; i < wordNumber; i++) {
+                let randomIndex = Math.floor(Math.random() * wordLists.length)
+                paragraph.push(wordLists[randomIndex])
+            }
+            return setParagraph(paragraph)
+        }
+    }, [generateTriger])
+
+    return <div>
+        {paragraph.join(" ")}
+    </div>
+}
+
+export default function ParagraphGenerator() {
+    const [generate, setGenerate] = useState(0)
+
+    return (
+        <div>
+            <Input onGenerate={() => setGenerate(prev => prev + 1)} />
+            <ParagraphDisplay generateTriger={generate}/>
+
+        </div>
+    )
 }
