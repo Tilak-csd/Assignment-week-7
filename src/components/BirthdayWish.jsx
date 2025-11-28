366dc533ec7951c90a696f41c7335345e0d25af7
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-28 14:10:24 +0545

    hello

--- src/components/BirthdayWish.jsx
+++ src/components/BirthdayWish.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import background from '../assets/images/background.jpg'
 export default function BirthdayWish() {
   return (
-    <div className='w-full h-[100vh]' style={{backgroundImage :`url(${background})`}}>
-      
+    <div className='w-full h-[100vh] flex justify-center items-center blur-md absolute z-0' style={{backgroundImage :`url(${background})`}}>
+    
     </div>
   )
 }
